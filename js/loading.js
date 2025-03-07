c4a629089a64ebe4f0cce605a0bacb7e6e387ecf
AudreyLenard <audrey.lenard517@gmail.com>
2025-03-07 21:22:02 +0000

    Fri, Mar 7, 2025, 1:22 PM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,9 +1,12 @@
 const loadText = document.querySelector('.loading-text')
 const bg = document.querySelector('.bg')
 
+
 let load = 0
 
+
 let int = setInterval(blurring, 30)
+
 
 function blurring() {
     load = load + 1
@@ -12,8 +15,14 @@ function blurring() {
         clearInterval(int)
     }
 
+
     loadText.innerText = `${load}%`
     bg.style.filter = `blur(${50 - (load / 2)}px)`
     loadText.style.opacity = `${1 - load/100}`
 
+
 }
+
+
+
+
