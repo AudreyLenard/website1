2527bdf95ec57fe75d1faab57bea570bccdc8abd
AudreyLenard <audrey.lenard517@gmail.com>
2025-03-31 17:00:39 +0000

    Mon, Mar 31, 2025, 10:00 AM -07:00

--- js/relaxer.js
+++ js/relaxer.js
@@ -1,4 +1,21 @@
 const container = document.getElementById('container')
 const text = document.getElementById('text')
 
-function
+breatheAnimation()
+breatheAnimation()
+
+function breatheAnimation() {
+    text.innerHTML = 'Breathe In'
+    container.classList.remove('shrink')
+    container.classList.add('grow')
+
+    setTimeout( () => {
+        text.innerHTML = 'Hold'
+    }, 3000)
+
+    setTimeout( () => {
+        text.innerHTML = 'Breathe out!'
+        container.classList.remove('grow')
+        container.classList.add('shrink')
+    }, 4500)
+}
