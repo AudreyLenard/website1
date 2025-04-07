662679692688c64e38fff5c1afc5326a14acd066
AudreyLenard <audrey.lenard517@gmail.com>
2025-04-07 17:06:04 +0000

    Mon, Apr 7, 2025, 10:06 AM -07:00

--- js/ball.js
+++ js/ball.js
@@ -15,7 +15,12 @@ setInterval(moveBall, 10)
 
 function moveBall(){
     ballXPosition = ballXPosition + ballSpeed * ballXDirection
+    ballYPosition = ballYPosition + ballSpeed * ballYDirection
     ball.style.left = `${ballXPosition}px`
+    ball.style.top = `${ballYPosition}px`
+    if (ballYPosition < 0 || ballYPosition > windowHeight - 2 * ballRadius){
+        ballYDirection = ballYDirection * -1
+    }
     if (ballXPosition < 0 || ballXPosition > windowWidth - 2*ballRadius){
         ballXDirection = ballXDirection * -1
     }
